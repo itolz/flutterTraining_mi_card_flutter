6f39e79120ffdae06aaa1a3fcf056e0da1349779
Italo Vinicios <italovinicios@gmail.com>
2019-11-15 19:48:17 -0300

    trabalho com containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Center(child: Text('Novo App do Tolz')),
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+             crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              height: 100.0,
+              color:  Colors.blue,
+              child: Text('Container 2'),
+
+            ),
+                        Container(
+              height: 100.0,
+              color:  Colors.red,
+              child: Text('Container 3'),
+
+            ),
+            Container(width: double.infinity, height: 10.0,)
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
