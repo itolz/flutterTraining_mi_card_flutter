b2fef628b2f59ff07640e3af2a375916ece9cca7
Italo Vinicios <italovinicios@gmail.com>
2019-11-17 21:22:49 -0300

    flutter Layout Challenge Completado

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Center(child: Text('Novo App do Tolz')),
-        ),
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-             crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            SizedBox(height: 20.0,),
-            Container(
-              height: 100.0,
-              color:  Colors.blue,
-              child: Text('Container 2'),
-
-            ),
-                        Container(
-              height: 100.0,
-              color:  Colors.red,
-              child: Text('Container 3'),
-
-            ),
-            Container(width: double.infinity, height: 10.0,)
-          ],
-        )),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.greenAccent,
+              ),
+                ],
+              ),
+              
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
